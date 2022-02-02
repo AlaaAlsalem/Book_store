cbeac95f4b492bb12ffe38c05fc9e8f0b4d0d519
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-02-02 20:35:27 +0200

    Add Books componenet, default view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Books from './components/Books'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello react</h1>
+        <Books/>
       </header>
     </div>
   );
---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+
+const Books =()=>{
+    return(
+        <div>
+            <h1> this is books</h1>
+            <section className="book_list">
+                
+            </section>
+        </div>
+    );
+}
+
+export default Books;
