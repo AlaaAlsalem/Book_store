455738d32b23dc04937e58d5abce9d6313588774
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-02-04 23:08:53 +0200

    Fix linter errors

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../redux/Books/books';
+import { addBook, removeBook } from '../redux/books/books';
 import Book from './Book';
 import BookInput from './BookInput';
 
